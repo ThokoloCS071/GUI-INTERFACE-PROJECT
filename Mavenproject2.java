34ff1047234ef3a72ecb4e4ea03f3583b179665f
Mr Thokolo CS <thokolosc@gmail.com>
2026-04-29 16:42:34 +0200

    add in one folder Gui interface

---
+++ Mavenproject2.java
@@ -0,0 +1,14 @@
+package com.mycompany.mavenproject2;
+
+/**
+ * @author THOKOLO CHUENE SIMON
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        // ADDED CODE: Create and show the first JFrame
+        NewJFrame firstFrame = new NewJFrame();
+        firstFrame.setVisible(true);
+        firstFrame.setLocationRelativeTo(null); // Centers the window
+    }
+}
